b4131ac63e7630e7f396a88f7c836f12a85c6b29
Aleksandr Petrov <aleksandr.petrov@x5.ru>
2021-09-25 11:35:55 +0300

    refactoring exchange_rate.py

--- exchange_rate.py
+++ exchange_rate.py
@@ -1,6 +1,7 @@
 from pprint import pprint
 import requests
 
+
 class Currency:
     def __init__(self, type):
         self.type = type.upper()
@@ -13,13 +14,8 @@ class Currency:
         return response.json()['Valute'][self.type]['Value']
 
 
-date = '2021/03/25'
-url = f'https://www.cbr-xml-daily.ru/archive/{date}/daily_json.js'
-# Отправляем запрос на указанный url
-response = requests.get(url)
-pprint(response.json()['Valute']['USD']['Value'])
-pprint(response.json()['Valute']['EUR']['Value'])
-
-# Если хотим получить курсы на определенную дату, то нужно отправить запрос на :
-# url: https://www.cbr-xml-daily.ru/archive/2021/04/07/daily_json.js
-# где /2021(год)/04(месяц)/07(день)/
+usd = Currency("usd")  # Создаем валюту "Доллар"
+euro = Currency("eur")  # Создаем валюту "Евро"
+print(usd['02.09.2020'])  # ← получение курса доллара на указанную дату
+print(euro['12.10.2018'])  # ← получение курса евро на указанную дату
+print(euro['12.12.2018'])  # ← в случае некорректной выбрасываем исключение
